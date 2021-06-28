9422a28a794d7509617da227f50c3ba449f27910
Mattia Martello <jhonnyc98.jc@gmail.com>
2021-06-28 17:18:28 +0200

    feature created

--- src/main/java/TAASS/ServiceDBUtenti/security/config/WebSecurityConfig.java
+++ src/main/java/TAASS/ServiceDBUtenti/security/config/WebSecurityConfig.java
@@ -1,8 +1,11 @@
 package TAASS.ServiceDBUtenti.security.config;
 
 
+import TAASS.ServiceDBUtenti.models.CustomOAuth2User;
 import TAASS.ServiceDBUtenti.security.token.IJwtTokenProviderService;
 import TAASS.ServiceDBUtenti.security.token.JwtTokenFilterConfigurer;
+import TAASS.ServiceDBUtenti.services.CustomOAuth2UserService;
+import TAASS.ServiceDBUtenti.services.SecureUserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,8 +14,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 
 @Configuration
 @EnableWebSecurity
@@ -21,8 +32,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private IJwtTokenProviderService jwtTokenProviderService;
 
-    public WebSecurityConfig(IJwtTokenProviderService jwtTokenProviderService) {
+    //private SecureUserService secureUserService;
+
+
+    //@Autowired
+    //private CustomOAuth2UserService oauthUserService;
+
+    public WebSecurityConfig(IJwtTokenProviderService jwtTokenProviderService, CustomOAuth2UserService oauthUserService/*, SecureUserService secureUserService*/) {
         this.jwtTokenProviderService = jwtTokenProviderService;
+        //this.oauthUserService = oauthUserService;
+        //this.secureUserService = secureUserService;
     }
 
     @Override
@@ -31,23 +50,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        // Entry points
-        //TODO: verificare che tutto funzioni ancora eliminando questa parte
-        /*http.authorizeRequests()
-                .antMatchers("/api/v1/login").permitAll()
-                .antMatchers("/api/v1/signUp").permitAll()
-                .antMatchers("/api/v1/validateToken").permitAll()
-                .antMatchers("/api/v1/utente/getUser/**").permitAll()
-                .antMatchers("/api/v1/utente/getUsersByIdList/**").permitAll()
-                .anyRequest().authenticated();
+        http.authorizeRequests()
+                .antMatchers("/", "/login", "/oauth/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()//.permitAll()
+                .and()
+                .oauth2Login()
+                .loginPage("/login")
+                .userInfoEndpoint()
+                //.userService(oauthUserService)
+                .and()
+                .successHandler(new AuthenticationSuccessHandler() {
 
-        http.logout(logout-> logout
-        .logoutUrl("/api/v1/logout")
-        .invalidateHttpSession(true)
-        );
+                    @Override
+                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                        Authentication authentication) throws IOException, ServletException {
 
-        // If a user try to access a resource without having enough permissions
-        http.exceptionHandling().accessDeniedPage("/accessDeniedPage");*/
+                        CustomOAuth2User oauthUser = (CustomOAuth2User) authentication.getPrincipal();
+
+                        //secureUserService.processOAuthPostLogin(oauthUser.getEmail());
+
+                        response.sendRedirect("/list");
+                    }
+                })
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403");
 
         // Apply JWT
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProviderService));
